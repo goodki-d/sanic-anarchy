e7316b26bfbdf994cccded307d74ce7ca35d45cb
goodki-d <185676088+goodki-d@users.noreply.github.com>
2025-01-19 22:21:18 +0530

    XD

---
+++ sanic_anarchy/__init__.py
@@ -0,0 +1,43 @@
+"""
+Brings back the og sanic logo.
+
+```python
+import sanic_anarchy as anarchy; anarchy.patch()
+```
+
+"""
+
+from sanic import Sanic
+from sanic.log import logger
+
+
+OG_SANIC = """
+                 ▄▄▄▄▄
+        ▀▀▀██████▄▄▄       _______________
+      ▄▄▄▄▄  █████████▄  /                 \\
+     ▀▀▀▀█████▌ ▀▐▄ ▀▐█ |   Gotta go fast!  |
+   ▀▀█████▄▄ ▀██████▄██ | _________________/
+   ▀▄▄▄▄▄  ▀▀█▄▀█════█▀ |/
+        ▀▀▀▄  ▀▀███ ▀       ▄▄
+     ▄███▀▀██▄████████▄ ▄▀▀▀▀▀▀█▌
+   ██▀▄▄▄██▀▄███▀ ▀▀████      ▄██
+▄▀▀▀▄██▄▀▀▌████▒▒▒▒▒▒███     ▌▄▄▀
+▌    ▐▀████▐███▒▒▒▒▒▐██▌
+▀▄▄▄▄▀   ▀▀████▒▒▒▒▄██▀
+          ▀▀█████████▀
+        ▄▄██▀██████▀█
+      ▄██▀     ▀▀▀  █
+     ▄█             ▐▌
+ ▄▄▄▄█▌              ▀█▄▄▄▄▀▀▄
+▌     ▐                ▀▀▄▄▄▀
+ ▀▀▄▄▀
+"""
+
+
+def patch():
+    """Patch motd to display og sanic."""
+
+    def new_motd(*_, **__):
+        logger.debug(OG_SANIC)
+
+    Sanic.motd = new_motd
